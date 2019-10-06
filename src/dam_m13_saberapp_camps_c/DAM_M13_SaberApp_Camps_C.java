64e110ba10e1eca0a35d9672d3512b2c9df7fcf5
Montse <m_camps7@hotmail.com>
2019-10-06 11:46:58 +0200

    prova canvi

--- src/dam_m13_saberapp_camps_c/DAM_M13_SaberApp_Camps_C.java
+++ src/dam_m13_saberapp_camps_c/DAM_M13_SaberApp_Camps_C.java
@@ -15,7 +15,7 @@ public class DAM_M13_SaberApp_Camps_C {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hola món");
     }
     
 }
